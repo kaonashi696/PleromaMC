ccd70947d578cd98edefd93773227b5a4548c055
BuildTools <unconfigured@null.spigotmc.org>
2021-04-26 00:21:31 +0200

    changed death message for died players with empty inventory

--- src/com/kaonashi696/pleromamc/listeners/PlayerDeathListener.java
+++ src/com/kaonashi696/pleromamc/listeners/PlayerDeathListener.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
@@ -22,16 +24,27 @@ public class PlayerDeathListener implements Listener{
 	  this.core = core;
 	  
 	}
-
-	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event) throws IOException {
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        
+        Bukkit.getScheduler().runTaskAsynchronously(PleromaMC.getPlugin(), () -> {
+			try {
+				runAsync(event);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+    }
+	
+	private void runAsync(PlayerDeathEvent event) throws IOException {
 		
 		FileConfiguration config = core.getConfig();
 		String post_url = config.getString("post_url") + "api/v1/statuses";
 		
 		String deathMessage = event.getDeathMessage();
 		List<ItemStack> drops = event.getDrops();
-		//String dropsMessage = StringUtils.join(drops, ", ");
 		
 		String items = drops.stream()
 				.map(item -> String.format(":%s: x%d", item.getType().toString().toLowerCase(), item.getAmount()))
@@ -39,7 +52,12 @@ public class PlayerDeathListener implements Listener{
 		
 		if (StringUtils.isBlank(deathMessage)) return;
 		
-		HTTPSPostRequest.sendPOST(core, post_url, "status=:skull_crossbones: #" + deathMessage + " and dropped " + items);
+		boolean ans = drops.isEmpty();
+        if (ans == true)
+    		HTTPSPostRequest.sendPOST(core, post_url, "status=:skull_crossbones: #" + deathMessage + " and didn't drop anything.");
+        else
+    		HTTPSPostRequest.sendPOST(core, post_url, "status=:skull_crossbones: #" + deathMessage + " and dropped " + items);
+		
 	}
 	
 
